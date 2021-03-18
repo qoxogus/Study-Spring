a2528eb0003db2e4db558e1a81363959456daf7f
qoxogus <a01034367055@gmail.com>
2021-03-18 14:23:40 +0900

    [정적 컨텐츠, MVC template엔진]

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,8 +11,13 @@ public class HelloController {
 
     @GetMapping("hello") //HTTP 메서드 ex) GET, POST
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!");  // hello!! == 보내는 값  (바뀔수있는 값임)
+        model.addAttribute("data", "hello!!");  // hello!! == 보내는 값  (바뀔수있는 값임)    model에 hello!! 담는다
         return "hello"; //return시 model(data : hello!!)  hello -> resources/templates/hello.html
+    }
 
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name")  String name, Model model) { //name이라는 파라미터를 받는다 ( localhost:8080/hello-mvc?name=spring ) {spring}
+        model.addAttribute("name", name); //model에 이름{spring}을 담는다
+        return "hello-template"; //model로 hello-template.html로 반환
     }
 }
